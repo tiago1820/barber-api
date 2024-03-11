536781d699d85689393670c9d0e5d9078f13b8ec
tiago1820 <tiago.zdo@gmail.com>
2024-03-11 15:06:24 -0300

    The list method was created to obtain all the barbers.

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ Route::get('/ping', function() {
 
 Route::get('/401', [AuthController::class, 'unauthorized'])->name('login');
 
-Route::get('/random', [BarberController::class, 'createRandom']);
+// Route::get('/random', [BarberController::class, 'createRandom']);
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::post('/auth/logout', [AuthController::class, 'logout']);
